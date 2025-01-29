e9b9d8937b092b508cc9753189af44f3798d1702
Leandro Balaban <126489808+leandro1027@users.noreply.github.com>
2025-01-29 19:56:33 -0300

    Update listar-depoimentos.php

--- painel/pages/listar-depoimentos.php
+++ painel/pages/listar-depoimentos.php
@@ -1,1 +1,18 @@
-<h2>Listagem de dpeoimentos</h2>
+<div class="box-content">
+    <h2><i class="fas fa-database"></i> Depoimentos cadastrados</h2>
+
+    <table>
+        <tr>
+            <td>Data</td>
+            <td>Nome</td>
+            <td>Editar</td>
+            <td>Excluir</td>
+        </tr>
+        <tr>
+            <td>29/01/2025</td>
+            <td>Leandro</td>
+            <td><a class="edit" href=""><  <i class="fas fa-edit" ></i></a></td>
+            <td><a class="delet" href=""><  <i class="fas fa-trash" ></i></a></td>
+        </tr>
+    </table>
+</div>
