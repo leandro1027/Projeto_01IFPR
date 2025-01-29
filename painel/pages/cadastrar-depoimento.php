e03725928ceb95a00c1ba29831e2b81bd4509eaf
Leandro Balaban <126489808+leandro1027@users.noreply.github.com>
2025-01-29 19:36:29 -0300

    Update cadastrar-depoimento.php

--- painel/pages/cadastrar-depoimento.php
+++ painel/pages/cadastrar-depoimento.php
@@ -1,2 +1,22 @@
 <div class="box-content"><
 <h2><i class ="fas fa-edit"></i> Adicionar Depoimentos</h2>/div>
+
+<form method="post" enctype="multipart/form-data">
+    <?php 
+    if(isset($_POST['acao'])){
+        Painel::messageToUser('sucesso', 'Depoimento cadastrado com sucesso!');
+    }
+    ?>
+    <div class="form-group">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome">
+    </div>
+    <div class="form-group">
+        <label for="nome">Depoimento:</label>
+        <textarea name="depoimento"></textarea>
+    </div>
+
+    <div class="form-group">
+        <input type="submit" name="acao" value="Cadastrar">
+    </div>
+</form>
