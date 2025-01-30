3c215c2db657417a49c2d2c83379150a69a8fcc4
Leandro Balaban <126489808+leandro1027@users.noreply.github.com>
2025-01-29 21:51:17 -0300

    Update listar-depoimentos.php

--- painel/pages/listar-depoimentos.php
+++ painel/pages/listar-depoimentos.php
@@ -20,15 +20,18 @@ $depoimentos = Painel::getAll('tb_admin.depoimentos', ($paginaAtual - 1)*$porPag
             <td>Nome</td>
             <td>Editar</td>
             <td>Excluir</td>
+            <td>Descer</td>
+            <td>Subir</td>
         </tr>
-        <?php foreach ($depoimentos as $key => $value) { ?>
+        <?php foreach ($depoimentos as $key => $value) { 
+        ?>
         <tr>
             <td><?php echo $value['data']; ?></td>
             <td><?php echo $value['nome']; ?></td>
-            <td><a class="edit" href="<?php echo INCLUDE_PATH_PAINEL ?>editar-depoimentos?id = <?php 
-                                        echo $value['id']; ?>"><i class="fas fa-edit"></i></a></td>
-            <td><a actionBtn ="delete" href="<?php echo INCLUDE_PATH_PAINEL ?>listar-depoimentos?excluir=<?php
-                                                 echo $value['id'];?>"><<i class="fas fa-trash"></i></a></td>
+            <td><a class="edit" href="<?php echo INCLUDE_PATH_PAINEL ?>editar-depoimentos?id = <?php echo $value['id']; ?>"><i class="fas fa-edit"></i></a></td>
+            <td><a actionBtn ="delete" href="<?php echo INCLUDE_PATH_PAINEL ?>listar-depoimentos?excluir=<?php echo $value['id'];?>"><<i class="fas fa-trash"></i></a></td>
+            <td><a class = "order-down" href=""> <i class="fa-solid fa-angle-down"></i></a></td>
+            <td><a class = "order-up" href=""> <i class="fa-solid fa-angle-up"></i></a></td>
         </tr>
         <?php } ?>
     </table>
