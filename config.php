c4b1081ce38c6fcb68ef227386d02cfb8dbe7325
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-05 12:24:21 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,65 +1,57 @@
 <?php
- 
-    /*cria uma sessão ou retorna o atual com base em um identificaor passado por
-     meio de uma solicitação GET ou POST ou passado por meio de um cookie*/
-    session_start();
+    session_start(); // Cria uma sessão ou retorna a atual com base em um identificador passado por meio de uma solicitação GET ou POST ou passado por meio de um cookie
+    
+    date_default_timezone_set('America/Sao_Paulo'); // Fuso horário de São Paulo
 
-    define('INCLUDE_PATH', 'http://localhost/Projeto_01testee/'); // Definir o domínio do site
+    define('NOME_EMPRESA', 'IFPR'); // Nome da Empresa
 
-    define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+    define('INCLUDE_PATH', 'http://localhost/Projeto_01/'); // Definir o domínio do site
+    
+    define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'painel/'); // Definir a URL do painel
 
-    //fuso horario de SP
-    date_default_timezone_set('America/Sao_Paulo');
-
-    define(constant_name: 'NOME_EMPRESA', 'IFPR');
-
-    define ('BASE_DIR_PAINEL', __DIR__.'/painel/');
+    define('BASE_DIR_PAINEL', __DIR__.'/painel/'); // Diretório base as imagens
 
     //Banco de dados
-    //Hospedagem
-    define('HOST', 'localhost');
-    //Banco
-    define('DATABASE', 'projeto_01');
-    //Usuario
-    define('USER', 'root');
-    //Senha
-    define('PASSWORD', '');
-
+    define('HOST', 'localhost'); //Hospedagem
+    define('DATABASE', 'projeto_01'); //Banco
+    define('USER', 'root'); //Usuario
+    define('PASSWORD', ''); //Senha
+    
     $autoload = function($class){
         include('assets/classes/'.$class.'.php'); // Carregando a classe: 'Email'
     };
 
     spl_autoload_register($autoload);
 
-    //função para os cargos dentro do painel
+    // Função para os cargos dentro do painel
     function pegaCargo($indice){
         return Painel::$cargos[$indice];
     }
 
+    // Função para o menu selecionado
     function selecionaMenu($menuItem){
-        $url = explode('/', @$_GET['url'])[0];
-        if ($url == $menuItem){
+        $url = explode('/',@$_GET['url'])[0];
+        if($url == $menuItem){
             echo 'class="menu-active"';
-
         }
     }
 
+    // Função para verificar a permissão do usuário
     function verificaPermissaoMenu($permissao){
-        if ($_SESSION['cargo'] >= $permissao){
+        if($_SESSION['cargo'] >= $permissao){
             return true;
         }else{
             echo 'style="display:none"';
         }
-        
     }
 
+    // Função para verificar a permissão para exibir a página na url
     function verificaPermissaoPagina($permissao){
-        if ($_SESSION['cargo'] >= $permissao) {
+        if($_SESSION['cargo'] >= $permissao){
             return true;
-    }else{
-        include('painel/pages/permissao_negada.php');
-        die();
+        }else{
+            include('painel/pages/permissao_negada.php');
+            die();
+        }
     }
-}
-
 ?>
