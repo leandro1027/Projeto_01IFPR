c507b8027df42d7d6df32ba250dfc957ad82fce4
Leandro Balaban <126489808+leandro1027@users.noreply.github.com>
2025-02-05 20:23:58 -0300

    Update listar-categorias.php

---
+++ painel/pages/listar-categorias.php
@@ -0,0 +1,62 @@
+<?php
+    if(isset($_GET['excluir'])){
+        $idExcluir = intval($_GET['excluir']);
+        Painel::delete('tb_admin.categorias', $idExcluir);
+        Painel::redirect(INCLUDE_PATH_PAINEL.'listar-categorias');
+    }else if(isset($_GET['order']) && isset($_GET['id'])){
+        Painel::orderItem('tb_admin.categorias', $_GET['order'], $_GET['id']);
+    }
+
+$paginaAtual = isset($_GET['pagina']) ? (int) $_GET['pagina'] : 1;
+$porPagina = 4;
+$slides = Painel::getAll('tb_admin.categorias', ($paginaAtual - 1) * $porPagina, $porPagina);
+?>
+
+<div class="box-content">
+    <h2><i class="fas fa-database"></i> categorias cadastrados</h2>
+
+    <div class="wraper-table">
+        <table>
+            <tr>
+                <td>Nome</td>
+                <td>Imagem</td>
+                <td>Editar</td>
+                <td>Excluir</td>
+                <td>Descer</td>
+                <td>Subir</td>
+            </tr>
+            <?php foreach ($categorias as $key => $value) { ?>
+            <tr>
+                <td><?php echo $value['nome']; ?></td>
+                <td><a class="edit"
+                        href="<?php echo INCLUDE_PATH_PAINEL ?>editar-categorias?id=<?php echo $value['id']; ?>"><i
+                            class="fas fa-edit"></i></a></td>
+                <td><a actionBtn="delete" class="delete"
+                        href="<?php echo INCLUDE_PATH_PAINEL ?>listar-categorias?excluir=<?php echo $value['id'];?>"><i
+                            class="fas fa-trash"></i></a></td>
+                <td><a class="order-down"
+                        href="<?php echo INCLUDE_PATH_PAINEL; ?>listar-categorias?order=up&id=<?php echo $value['id']; ?>"><i
+                            class="fa-solid fa-angle-down"></i></a></td>
+                <td><a class="order-up"
+                        href="<?php echo INCLUDE_PATH_PAINEL; ?>listar-categorias?order=down&id=<?php echo $value['id']; ?>"><i
+                            class="fa-solid fa-angle-up"></i></a></td>
+            </tr>
+            <?php } ?>
+        </table>
+    </div>
+    <!--wraper table-->
+
+    <div class="paginacao">
+        <?php 
+            $totalPaginas = ceil(count(Painel::getAll('tb_admin.depoimentos')) / $porPagina);
+            for ($i = 1; $i <= $totalPaginas ; $i++){
+                if($i == $paginaAtual)
+                    echo '<a class="page-selected" href="' . INCLUDE_PATH_PAINEL . 'listar-depoimentos?pagina=' . $i . '">' . $i . '</a';
+                else
+                    echo '<a href="' . INCLUDE_PATH_PAINEL . 'listar-depoimentos?pagina=' . $i . '">' . $i . '</a>';
+            }
+        ?>
+    </div>
+    <!--paginacao-->
+</div>
+<!--box content-->
