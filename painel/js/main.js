769778aa5a76525cf50dfde0da57488bf517fe7e
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-05 12:25:17 -0300

    .

--- painel/js/main.js
+++ painel/js/main.js
@@ -1,37 +1,35 @@
-$(function () {
+$(function(){
     var open = true;
     var windowSize = $(window)[0].innerWidth;
 
-    if (windowSize <= 768) {
-        open = false
-
+    if(windowSize <= 768){
+        open = false;
     }
 
-    $('.menu-btn').click(function () {
-        if (open) {
-            //menu esta aberto precisamos fechar
+    $('.menu-btn').click(function(){
+        if(open){
+            //Menu está aberto, precisamos fechar
             $('header, .content').css('width','100%');
-            $('header, .content').animate({left: '0'});
+            $('header, .content').animate({left:'0'});
             $('aside').fadeOut();
             open = false;
-        } else {
-            //menu esta fechado precisamos abrir
+        }else{
+            //Menu está fechado, precisamos abrir
             $('header, .content').css('width','calc(100% - 250px)');
             $('header, .content').animate({left:'250px'});
             $('aside').fadeIn();
             open = true;
         }
-
     })
 
     $('[formato="data"]').mask('99/99/9999');
-    $('[actionBtn=delete]').click(function (){
+
+    $('[actionBtn=delete]').click(function () {
         let text;
-        if (confirm("Press a button!") == true) {
-        text = "You pressed OK!";
+        if (confirm("Deseja excluir o registro?") == true) {
+            return true;
         } else {
-        text = "You canceled!";
+            return false;
         }
     })
-
 })
