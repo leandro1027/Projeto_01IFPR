784e5211d99de99705ed0d34188d23525e674d60
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-05 12:22:12 -0300

    .

--- assets/classes/usuario.php
+++ assets/classes/usuario.php
@@ -1,26 +1,24 @@
 <?php
     class Usuario{
-
         public function updateUser($nome, $password, $imagem){
-            $sql = MySql::conectar()->prepare("UPDATE `tb_admin.usuarios` set 
-            nome = ?, password = ?, img = ? WHERE user = ?");
-        if ($sql->execute(array($nome, $password, $imagem, $_SESSION['user']))) {
-            return true;
-        }
-        return false;
+            $sql = MySql::conectar()->prepare("UPDATE `tb_admin.usuarios` SET nome = ?, password = ?, img = ? WHERE user = ?");
+            if($sql->execute(array($nome, $password, $imagem, $_SESSION['user']))){
+                return true;
+            }
+            return false;
         }
 
         public static function userExists($login){
-            $sql = MySql::conectar()->prepare("SELECT `id` FROM `tb_admin.usuarios` WHERE user =?");
-            $sql = MySql::execute(array($login));
-            if($sql-rowCount() == 1)
+            $sql = MySql::conectar()->prepare("SELECT `id` FROM `tb_admin.usuarios` WHERE user = ?");
+            $sql->execute(array($login));
+            if($sql->rowCount() == 1)
                 return true;
             return false;
-
         }
-        public static function registerUser($login, $password, $nome, $cargo){
-            $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.usuarios` VALUES (null, ?,?,?,?,?");
-            $sql = MySql::execute(array($login, $password,'', $nome, $cargo));
+
+        public function registerUser($login, $password, $nome, $cargo){
+            $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.usuarios` VALUES (null,?,?,?,?,?)");
+            $sql->execute(array($login, $password, '', $nome, $cargo));
         }
     }
 ?>
