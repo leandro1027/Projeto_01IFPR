7381094d75a228d513b5ae93054b60118bbb6bff
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-05 12:44:41 -0300

    .

--- painel/pages/cadastrar-depoimento.php
+++ painel/pages/cadastrar-depoimento.php
@@ -1,33 +1,38 @@
-<div class="box-content"><
-<h2><i class ="fas fa-plus"></i> Adicionar Depoimentos</h2>/div>
 
-<form method="post" enctype="multipart/form-data">
-    <?php 
-    if(isset($_POST['acao'])){
-        if (Painel::insert($_POST)) {
-            Painel::messageToUser('sucesso','Depoimento cadastrado com sucesso!');
-        }else{
-        Painel::messageToUser('erro', 'Não foi possivel cadastrar o depoimento!');
+<div class="box-content">
+    <h2><i class="fas fa-plus"></i> Adicionar Depoimento</h2>
+
+    <form method="post" enctype="multipart/form-data">
+        <?php 
+        if (isset($_POST['acao'])){
+            if(Painel::insert($_POST)){
+            Painel::messageToUser('sucesso', 'Depoimento cadastrado com sucesso!');
+            }else{
+                Painel::messageToUser('erro', 'Não foi possível cadastrar o depoimento');
+            }
         }
-    }
-    ?>
-    <div class="form-group">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome">
-    </div>
-    <div class="form-group">
-        <label for="nome">Depoimento:</label>
-        <textarea name="depoimento"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="nome">Data:</label>
-        <input fomato="data" type="text" name="data" required>
-    </div>
-
-
-    <div class="form-group">
-        <input type="hidden" name="order_id" value="0">
-        <input type="hidden" name="nomeTabela" value="tb_admin.depoimentos">
-        <input type="submit" name="acao" value="Cadastrar">
-    </div>
-</form>
+        ?>
+        <div class="form-group">
+            <label for="nome">Nome: </label>
+            <input type="text" name="nome" required>
+        </div>
+        <!--form group-->
+        <div class="form-group">
+            <label for="nome">Depoimento: </label>
+            <textarea name="depoimento" required></textarea>
+        </div>
+        <!--form group-->
+        <div class="form-group">
+            <label for="nome">Data: </label>
+            <input formato="data" type="text" name="data" required>
+        </div>
+        <!--form group-->
+        <div class="form-group">
+            <input type="hidden" name="order_id" value="0">
+            <input type="hidden" name="nomeTabela" value="tb_admin.depoimentos">
+            <input type="submit" name="acao" value="Cadastrar">
+        </div>
+        <!--form group-->
+    </form>
+</div>
+<!--box content-->
